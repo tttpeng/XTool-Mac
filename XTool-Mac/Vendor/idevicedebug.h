916e0e7def65f69b5a47557d886f8169ae77399a
tpeng <tttpeng.com@gmail.com>
2018-02-08 13:32:42 +0800

    [ADD] debug

---
+++ XTool-Mac/Vendor/idevicedebug.h
@@ -0,0 +1,14 @@
+//
+//  idevicedebug.h
+//  XTool-Mac
+//
+//  Created by tpeng on 2018/2/8.
+//  Copyright © 2018年 tpeng. All rights reserved.
+//
+
+#ifndef idevicedebug_h
+#define idevicedebug_h
+
+#include <stdio.h>
+
+#endif /* idevicedebug_h */
